32958d51fc7de1006cdd452b108da019b550cc72
Benjamin Akinlolu <geniuskhid1@gmail.com>
2026-05-06 15:21:26 +0100

    feat: create user Mongoose schema with authentication and profile fields

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
@@ -51,22 +50,5 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
-
-// Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-// Method to check password
-userSchema.methods.comparePassword = async function (candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
-};
-
-const User = mongoose.model('User', userSchema);
 
 module.exports = User;
