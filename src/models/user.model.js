ad09f307237ec9fa7026e7d55b15f55dde7b439e
Benjamin Akinlolu <geniuskhid1@gmail.com>
2026-05-08 13:04:11 +0100

    feat: initialize User Mongoose model with authentication and password hashing methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -51,6 +51,21 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Method to check password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
